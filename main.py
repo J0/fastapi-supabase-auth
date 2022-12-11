c11e0520917060149c572e955efe89881884e334
joel@joellee.org <joel@joellee.org>
2022-12-11 20:04:06 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,59 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi_sqlalchemy import DBSessionMiddleware, db
+
+from schema import Book as SchemaBook
+from schema import Author as SchemaAuthor
+
+from schema import Book
+from schema import Author
+
+from models import Book as ModelBook
+from models import Author as ModelAuthor
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv('.env')
+
+
+app = FastAPI()
+
+# to avoid csrftokenError
+app.add_middleware(DBSessionMiddleware, db_url=os.environ['DATABASE_URL'])
+
+@app.get("/")
+async def root():
+    return {"message": "hello world"}
+
+
+@app.post('/book/', response_model=SchemaBook)
+async def book(book: SchemaBook):
+    db_book = ModelBook(title=book.title, rating=book.rating, author_id = book.author_id)
+    db.session.add(db_book)
+    db.session.commit()
+    return db_book
+
+@app.get('/book/')
+async def book():
+    book = db.session.query(ModelBook).all()
+    return book
+
+
+  
+@app.post('/author/', response_model=SchemaAuthor)
+async def author(author:SchemaAuthor):
+    db_author = ModelAuthor(name=author.name, age=author.age)
+    db.session.add(db_author)
+    db.session.commit()
+    return db_author
+
+@app.get('/author/')
+async def author():
+    author = db.session.query(ModelAuthor).all()
+    return author
+
+
+# To run locally
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8000)
